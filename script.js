28ac1028a263b1f24a8817042f6ea15cff1c93dd
Kumar Pranjul <103567403+pranjul56667@users.noreply.github.com>
2023-04-25 00:36:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,169 @@
+let aUsersturns = [];
+let aDealerturns = [];
+
+let userscore = 5;
+let dealerscore = 5;
+//---------------------------------------------------------------------------------------
+//-----------------drawing the card function---------------------------------------------
+//---------------------------------------------------------------------------------------
+function drawcards() {
+    //-----------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------
+    var dealercard = Math.floor(Math.random() * 13 + 2);
+    aDealerturns.push(dealercard);
+
+    var dealercardoutput = "";
+    if(dealercard === 11) {
+        dealercardoutput = "J"
+    } else if(dealercard === 12) {
+        dealercardoutput = "Q";
+    } else if(dealercard === 13) {
+        dealercardoutput = "K";
+    } else if(dealercard ===14) {
+        dealercardoutput = "A";
+    } else {
+        dealercardoutput = dealercard;
+    }
+
+    document.getElementById("dealercardnum").innerHTML = dealercardoutput;
+    //-----------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------
+    var usercard = Math.floor(Math.random() * 13 + 2);
+    aUsersturns.push(usercard);
+
+    var usercardoutput = "";
+    if(usercard === 11) {
+        usercardoutput = "J"
+    } else if(usercard === 12) {
+        usercardoutput = "Q";
+    } else if(usercard === 13) {
+        usercardoutput = "K";
+    } else if(usercard === 14) {
+        usercardoutput = "A";
+    } else {
+        usercardoutput = usercard;
+    }
+
+    document.getElementById("usercardnum").innerHTML = usercardoutput;
+    //-----------------------------------------------------------------------------
+    //---------------------creting icons for dealer card---------------------------
+    //-----------------------------------------------------------------------------
+    var dealerchar = Math.floor(Math.random() * 4 + 1);
+
+    switch(dealerchar) {
+        case 1:
+            dealerchar = "&hearts;";
+            document.getElementById("dealerchar").style.color = "red";
+            document.getElementById("dealerbigchar").style.color = "red";
+        break;
+
+        case 2:
+            dealerchar = "&spades;";
+            document.getElementById("dealerchar").style.color = "black";
+            document.getElementById("dealerbigchar").style.color = "black";
+        break;
+
+        case 3:
+            dealerchar = "&diams;";
+            document.getElementById("dealerchar").style.color = "red";
+            document.getElementById("dealerbigchar").style.color = "red";
+        break;
+        
+        case 4:
+            dealerchar = "&clubs;";
+            document.getElementById("dealerchar").style.color = "black";
+            document.getElementById("dealerbigchar").style.color = "black";
+        break;
+    }
+    document.getElementById("dealerchar").innerHTML = dealerchar;
+    document.getElementById("dealerbigchar").innerHTML = dealerchar;
+    //------------------------------------------------------------------------------
+    //----------------creating icons for user card----------------------------------
+    //------------------------------------------------------------------------------
+    var userchar = Math.floor(Math.random() * 4 + 1);
+
+    switch(userchar) {
+        case 1:
+            userchar = "&hearts;";
+            document.getElementById("userchar").style.color = "red";
+            document.getElementById("userbigchar").style.color = "red";
+        break;
+
+        case 2:
+            userchar = "&spades;";
+            document.getElementById("userchar").style.color = "black";
+            document.getElementById("userbigchar").style.color = "black";
+        break;
+
+        case 3:
+            userchar = "&diams;";
+            document.getElementById("userchar").style.color = "red";
+            document.getElementById("userbigchar").style.color = "red";
+        break;
+        
+        case 4:
+            userchar = "&clubs;";
+            document.getElementById("userchar").style.color = "black";
+            document.getElementById("userbigchar").style.color = "black";
+        break;
+    }
+    document.getElementById("userchar").innerHTML = userchar;
+    document.getElementById("userbigchar").innerHTML = userchar;
+    //-----------------------------------------------------------------------------------
+    //------------------------display scores---------------------------------------------
+    //-----------------------------------------------------------------------------------
+    if(usercard === dealercard) {
+        document.getElementById("pointer").innerHTML = "=";
+    } else if(usercard > dealercard) {
+        userscore++;
+        dealerscore--;
+        document.getElementById("pointer").innerHTML = "►";
+    } else if(usercard < dealercard) {
+        dealerscore++;
+        userscore--;
+        document.getElementById("pointer").innerHTML = "◄";
+    }
+
+    if(userscore < 2) {
+        document.getElementById("userscore").style.color = "red";
+    } else {
+        document.getElementById("userscore").style.color = "black";
+    }
+
+    if(dealerscore < 2) {
+        document.getElementById("dealerscore").style.color = "red";
+    } else {
+        document.getElementById("dealerscore").style.color = "black";
+    }
+    document.getElementById("dealerscore").innerHTML = dealerscore;
+    document.getElementById("userscore").innerHTML = userscore;        
+    //----------------------------------------------------------------------------------
+    //--------------------Display Turns-------------------------------------------------
+    //----------------------------------------------------------------------------------
+    var dealerturns = "";
+    var i;
+    for(i = 0; i < aDealerturns.length; i++) {
+        dealerturns = dealerturns + aDealerturns[i] + ", ";
+        document.getElementById("dealerturns").innerHTML = dealerturns
+    }
+
+    var userturns = "";
+    var j;
+    for(j = 0; j < aUsersturns.length; j++) {
+        userturns = userturns + aUsersturns[j] + ", ";
+        document.getElementById("userturns").innerHTML = userturns
+    }
+    chekingscores();
+}
+
+function chekingscores() {
+    if(userscore <= 0) {
+        userscore = 0;
+        alert("Dealer Win!");
+        document.getElementById("mainbtn").disabled = true;
+    } else if(dealerscore <= 0) {
+        dealerscore = 0;
+        alert("User Win!");
+        document.getElementById("mainbtn").disabled = true;
+    }
+}
